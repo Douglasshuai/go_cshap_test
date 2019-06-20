1ac43cb386ae81edacece3fcf177d16c3ac71fa7
sea <763424515@qq.com>
2019-06-20 20:03:31 +0800

    Client

---
+++ Client/Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Grpc.Core;
+
+namespace Test
+{
+    class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                Channel channel = new Channel("192.168.237.130:50051", ChannelCredentials.Insecure);
+                var client = new AccountService.AccountServiceClient(channel);
+
+                EmployeeName empName = client.GetEmployeeName(new EmployeeNameRequest { EmpId = "1" });
+
+                if(empName == null || string.IsNullOrWhiteSpace(empName.FirstName) ||
+                    string.IsNullOrWhiteSpace(empName.LastName))
+                {
+                    Console.WriteLine("Employee not found.");
+                }
+                else
+                {
+                    Console.WriteLine($"The employee name is {empName.FirstName} {empName.LastName}.");
+                }
+                channel.ShutdownAsync().Wait();
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Exception encountered: {ex}");
+            }
+        }
+    }
+}
